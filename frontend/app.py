297916767b9f15d6e63f4540011fbe642802defb
fdajuste52 <55099950+fdajuste52@users.noreply.github.com>
2020-04-09 22:01:59 -0400

    Add files via upload

--- frontend/app.py
+++ frontend/app.py
@@ -2,5 +2,47 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def anything():
-        return 'anything'
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        msg = messaging.Messaging()
+        msg.send(
+            'REGISTER',
+            {
+                'email': email,
+                'hash': generate_password_hash(password)
+            }
+        )
+        response = msg.receive()
+        if response['success']:
+            session['email'] = email
+            return redirect('/fashionsite_mainpage_IT490.htm')
+        else:
+            return f"{response['Unable to register user']}"
+    return render_template('fashionsite_register.html')
+
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        msg = messaging.Messaging()
+        msg.send('GETHASH', { 'email': email })
+        response = msg.receive()
+        if response['success'] != True:
+            return "Login failed."
+        if check_password_hash(response['hash'], password):
+            session['email'] = email
+            return redirect('/')
+        else:
+            return "Login failed."
+    return render_template('fashionsite_login.html')
+	
+def login_required(f):
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'email' not in session:
+            return redirect('/fashionsite_login.html')
+        return f(*args, **kwargs)
+    return decorated_function
