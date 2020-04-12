b9c6a936750511b4f040c2a9fb5ea2d94dd5c099
fdajuste52 <55099950+fdajuste52@users.noreply.github.com>
2020-04-12 11:17:59 -0400

    Update front_messaging.py

--- Shared/frontend/frontend/html/front_messaging.py
+++ Shared/frontend/frontend/html/front_messaging.py
@@ -1,12 +1,12 @@
 import pika
 import json
-import time
 import logging
+import time
 import os
 
-class Messaging:
+class messaging:
   
-    request_queue_name = 'request'
+    request_queue_name = 'requesting data'
 
 
     credentials = pika.PlainCredentials(os.environ['RABBITMQ_DEFAULT_USER'],
@@ -15,40 +15,40 @@ class Messaging:
     # docker-compose will resolve this host to our messaging service
     host = 'messaging'
 
-    def __init__(self):
+    def __init__(s):
      
         logging.info("Messaging: Establishing connection")
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.host, credentials=self.credentials))
-        self.channel = self.connection.channel()
+        s.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=s.host, credentials=s.credentials))
+        s.channel = s.connection.channel()
         logging.info("Messaging: Creating queues")
-        self.channel.queue_declare(queue=self.request_queue_name)
-        self.result_queue = self.channel.queue_declare(queue='', exclusive=True).method.queue
+        s.channel.queue_declare(queue=s.request_queue_name)
+        s.result_queue = s.channel.queue_declare(queue='', exclusive=True).method.queue
 
-    def __del__(self):
+    def __del__(s):
     
-        logging.info("Messaging: Closing down connection")
-        self.connection.close()
+        logging.info("Messaging: Connection will close down")
+        s.connection.close()
 
-    def send(self, action, data):
+    def send(s, action, data):
 
         logging.info(f"Messaging: send(action={action}, data={data})")
 
-        self.channel.basic_publish(
+        s.channel.basic_publish(
             exchange='',
-            routing_key=self.request_queue_name,
+            routing_key=s.request_queue_name,
             properties=pika.BasicProperties(
-                reply_to=self.result_queue),
+                reply_to=s.result_queue),
                 body=json.dumps({'action': action, 'data': data}
             )
         )
 
-    def receive(self):
+    def receive(s):
 
         attempts = 0
         while True:
-            method_frame, properties, body = self.channel.basic_get(
-                self.result_queue, auto_ack=True)
+            method_frame, properties, body = s.channel.basic_get(
+                s.result_queue, auto_ack=True)
             if method_frame:
                 received = json.loads(body)
                 logging.info(f"Messaging: received={received}")
